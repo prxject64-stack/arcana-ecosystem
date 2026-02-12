be25c1d52a7fd7c3cb4c8b9061874b7b51ea27a1
prxject64-stack <prxject64@gmail.com>
2026-02-12 22:52:41 +0000

    FINALIZE: Identity Layer verified. Nonce 4500. Gas audit passed. Ready for Good To GO.

--- register_peers.js
+++ register_peers.js
@@ -1,5 +1,5 @@
 var identityAddr = "0x73da448eccae7b5fa064f8d39cb97396e445fcc3";
-var fromAddr = "0xbdae565eff11d46dfd2154442f0a565b4011f3b8";
+var fromAddr = "0x5D815B693A80DED53fc19A9d8892B822895Ee6dD";
 var totalPeers = 100; // Starting with a test batch of 100
 
 console.log("Registering " + totalPeers + " peers to Identity Layer...");
