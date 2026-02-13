648ec81ee593001ab4abf8fb6dbadac06e36ff43
prxject64-stack <prxject64@gmail.com>
2026-02-13 06:50:03 +0000

    DEVOPS: Corrected Hardhat 3 network configuration schema

--- sauna-protocol/hardhat.config.ts
+++ sauna-protocol/hardhat.config.ts
@@ -5,10 +5,9 @@ const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     arcana: {
+      type: "http", // This is the missing discriminator required by Hardhat 3
       url: "http://127.0.0.1:8545",
-      // Use your Vault address private key here
-      accounts: ["0x995c7b87a714f4a018af1a3542af6463a4d8c1744a9155127a234fee3be55a49"], 
-      gasPrice: 1000000000, // 1 Gwei
+      accounts: ["0xYOUR_VAULT_PRIVATE_KEY"], 
     }
   }
 };
