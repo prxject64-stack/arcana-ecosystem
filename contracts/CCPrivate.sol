20e7101accf9dad8c4b20619d60ef89f2e3ff3f9
prxject64-stack <prxject64@gmail.com>
2026-02-16 21:11:44 +0000

    GENESIS: CC Token successfully deployed with London EVM compatibility. 136B supply live.

--- contracts/CCPrivate.sol
+++ contracts/CCPrivate.sol
@@ -1,16 +1,19 @@
- // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-contract CCPrivate {
-    string public name = "Credit Coin Private";
-    string public symbol = "CC";
-    uint8 public decimals = 18;
-    uint256 public totalSupply = 136000000000 * 10**18;
-    mapping(address => uint256) public balanceOf;
-    constructor() { balanceOf[msg.sender] = totalSupply; }
-    function transfer(address _to, uint256 _value) public returns (bool) {
-        require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-        return true;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CCPrivate is ERC20, Ownable {
+    // 136 Billion CC with 18 decimals
+    uint256 private constant INITIAL_SUPPLY = 136000000000 * 10**18;
+
+    constructor() ERC20("Credit Coin Private", "CC") Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+
+    // Pro Mode: Facilitate the 4,500 simultaneous swaps by allowing bulk approvals
+    function bulkApprove(address spender, uint256 amount) external {
+        _approve(msg.sender, spender, amount);
     }
 }
