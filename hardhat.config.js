d583b6004c9cb9d3a76956aa3ffb53663942ef43
prxject64-stack <prxject64@gmail.com>
2026-02-18 08:11:00 +0000

    MAINTENANCE: Restored deleted configs, stabilized Geth submodule, and cleaned repo state.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,11 @@
-import { task } from "hardhat/config";
-import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-toolbox";
 
-task("deploy-sauna", "Deploy Sauna", async (taskArgs, hre) => {
-  const [deployer] = await hre.ethers.getSigners();
-  console.log("--------------------------------------------------");
-  console.log("Verified Signer:", deployer.address);
-  const Factory = await hre.ethers.getContractFactory("SaunaPrivacyMask");
-  const contract = await Factory.deploy();
-  await contract.waitForDeployment();
-  console.log("SUCCESS! Address:", await contract.getAddress());
-  console.log("136B CC Liquidity Shield: ACTIVE");
-  console.log("--------------------------------------------------");
-});
-
-const config = {
+export default {
   solidity: "0.8.20",
   networks: {
     arcana: {
-      type: "http",
-      url: "http://127.0.0.1:8545",
-      accounts: ["0xfdfcd399e13c4f7e1612fd5e655159f899a4951ecf21d153a6adef68474271d2"]
+      url: "http://127.0.0.1:8547",
+      accounts: process.env.PEER2_PRIVATE_KEY ? [process.env.PEER2_PRIVATE_KEY] : [],
     }
   }
 };
-
-export default config;
