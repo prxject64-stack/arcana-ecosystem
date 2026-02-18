e4daa7e4e5f4447aaba77a3e9600574fd688b476
prxject64-stack <prxject64@gmail.com>
2026-02-18 08:39:17 +0000

    STABILITY: Fixed address checksum and restored module.exports.

--- sauna-protocol/hardhat.config.js
+++ sauna-protocol/hardhat.config.js
@@ -1,26 +1,22 @@
-import "@nomicfoundation/hardhat-toolbox";
+require('@nomicfoundation/hardhat-toolbox');
 
-export default {
-  solidity: {
-    compilers: [
-      { version: "0.8.28" },
-      { version: "0.8.20" },
-      { version: "0.8.19" },
-      { version: "0.7.6" },
-      { version: "0.6.12" },
-      { version: "0.5.16" }
-    ],
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
-      }
-    }
-  },
-  networks: {
-    arcana: {
-      url: process.env.ARCANA_RPC_URL || "http://127.0.0.1:8547",
-      accounts: process.env.PEER2_PRIVATE_KEY ? [process.env.PEER2_PRIVATE_KEY] : [],
-    }
-  }
-};
+task('dashboard').setAction(async(a,hre)=>{
+  const CC='0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+  const SUSD='0x84eA74d481Ee0A5332c457a4d796187F6Ba67fEB';
+  const TR='0x322813fd9a05da47206818293028c39ed8b00000';
+  const SMM='0x18E317A7D70d8fBf8e6E893616b52390EbBdb629';
+  const cc=await hre.ethers.getContractAt('contracts/IERC20.sol:IERC20',CC);
+  const ss=await hre.ethers.getContractAt('contracts/IERC20.sol:IERC20',SUSD);
+  const d=[
+    {E:'Treasury',C:await cc.balanceOf(TR),S:await ss.balanceOf(TR)},
+    {E:'SMM',C:await cc.balanceOf(SMM),S:await ss.balanceOf(SMM)}
+  ];
+  console.table(d.map(r=>({Entity:r.E,CC:hre.ethers.formatUnits(r.C,18),sUSD:hre.ethers.formatUnits(r.S,18)})));
+});
+
+task('balance').addParam('account').setAction(async(a,h)=>{
+  const t=await h.ethers.getContractAt('contracts/IERC20.sol:IERC20','0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512');
+  console.log(h.ethers.formatUnits(await t.balanceOf(a.account),18));
+});
+
+module.exports={solidity:'0.8.33',networks:{localhost:{url:'http://127.0.0.1:8545'}}};
