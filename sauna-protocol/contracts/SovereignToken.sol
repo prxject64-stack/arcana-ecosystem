ea720f61f728030e63d1cf2ecf170d056c30015b
prxject64-stack <prxject64@gmail.com>
2026-02-18 18:04:02 +0000

    GENESIS: Successfully rebranded and resurrected 146B ARC-S, CC-P, and saUSD.

--- sauna-protocol/contracts/SovereignToken.sol
+++ sauna-protocol/contracts/SovereignToken.sol
@@ -6,7 +6,7 @@ contract SovereignToken {
     string public name;
     string public symbol;
     uint8 public decimals = 18;
-    uint256 public totalSupply;
+    uint256 public totalSupply = 146000000000 * 10**18;
 
     constructor(string memory _name, string memory _symbol) {
         name = _name;
@@ -14,7 +14,7 @@ contract SovereignToken {
     }
 
     function transfer(address to, uint256 amount) public returns (bool) {
-        require(balanceOf[msg.sender] >= amount, "Balance too low");
+        require(balanceOf[msg.sender] >= amount, "Sovereign: Insufficient funds");
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
         return true;
