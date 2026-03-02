cee00bdcf809e66d4b36b87217d69524c5701342
prxject64-stack <prxject64@gmail.com>
2026-03-02 12:45:37 -0600

    System: Update Genesis and Miner with new Identity 0x218e

--- start_node.sh
+++ start_node.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-geth --datadir ./node_data --networkid 136 --cache 2048 --http --http.addr "127.0.0.1" --mine --miner.threads 1 --allow-insecure-unlock --unlock "0x4654cfdd1a9eaeaa43c985d5dfda6b4297a1e688" --password password.txt
+geth --datadir ./node_data \
+  --networkid 136 \
+  --mine \
+  --miner.etherbase "0x4654cfdd1a9eaeaa43c985d5dfda6b4297a1e688" \
+  --unlock "0x4654cfdd1a9eaeaa43c985d5dfda6b4297a1e688" \
+  --password password.txt \
+  --http --http.api "eth,net,web3,personal,miner" \
+  --allow-insecure-unlock \
+  --nodiscover \
+  --cache 2048
