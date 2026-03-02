824466e6fead1a2e58d209619b45a9ac43a58312
prxject64-stack <prxject64@gmail.com>
2026-03-02 12:27:52 -0600

    System: Stabilize repo; ignore node data and update start script

--- start_node.sh
+++ start_node.sh
@@ -1,1 +1,2 @@
-/usr/local/bin/geth --datadir /root/arcana-ecosystem/peer_2 --networkid 136 --http --http.addr 0.0.0.0 --http.port 8545 --http.api 'eth,net,web3,txpool,miner,admin,personal,clique,debug' --txpool.globalslots 10000 --unlock 0x4654cfdd1a9eaeaa43c985d5dfda6b4297a1e688 --password /root/arcana-ecosystem/password.txt --allow-insecure-unlock --mine --miner.etherbase 0x4654cfdd1a9eaeaa43c985d5dfda6b4297a1e688
+#!/bin/bash
+geth --datadir ./node_data --networkid 136 --cache 2048 --http --http.addr "127.0.0.1" --mine --miner.threads 1 --allow-insecure-unlock --unlock "0x4654cfdd1a9eaeaa43c985d5dfda6b4297a1e688" --password password.txt
